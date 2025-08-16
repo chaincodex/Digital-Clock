ed9e072036b04c7162414f71cc4f135ad027f205
NhiHoParaHaikY <hardwellmishra@gmail.com>
2025-08-16 11:40:41 +0530

    blured the clock

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const timeElement = document.getElementById("clock");
 
 function updateTime() {
     const now = new Date();
-    const hours = now.getHours();
+    let hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
+    const ampm = hours >= 12 ? "PM" : "AM";
 
-    const clockStr = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+
+    const clockStr = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')} ${ampm}`;
 
     timeElement.innerText = clockStr;
 }
